95b43750fed4c7f83b4b322c5bef336f652e545b
Drago Stevanovic <drago@sounder.fm>
2022-05-27 18:48:38 +0200

    Show only published posts from other users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function show(User $user)
     {
+        if (Auth::id() != $user->id) {
+            $user->load([
+                'posts' => function ($q) {
+                    return $q->published();
+                }
+            ]);
+        }
+
         return view('users.show', compact('user'));
     }
 }
